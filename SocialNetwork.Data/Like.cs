a95b6e4571a42691956584aa8b2b5d75e442210b
Ellie Hong <hongek9@gmail.com>
2022-01-25 18:21:54 -0500

    added data files and post models

---
+++ SocialNetwork.Data/Like.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Data
+{
+    public class Like
+    {
+        [Key]
+        public int LikeId { get; set; }
+        public Guid OwnerId { get; set; }
+
+        [ForeignKey(nameof(Post))]
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
